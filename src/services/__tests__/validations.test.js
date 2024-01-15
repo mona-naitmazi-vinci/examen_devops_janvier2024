191de57605e7775f9c5cdd1e8c0eb184940cb0cb
mona-naitmazi <mona.naitmazi@student.vinci.be>
2024-01-15 16:28:38 +0100

    tests

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,9 +1,24 @@
 // TODO: Create tests suite for validation function
 const { isValid } = require('../validations');
 
-describe('validations tests suites - isValid', () => {
-    test('should return true or false as the function is defined', () => {
-        const result = isValid();
-        expect(result).toBeDefined();
+describe('validations tests suites - isValid', () => {    
+    test('should return false as the name is too short', () => {
+        const result = isValid("sarah");
+        expect(result).toBeFalsy();
+    });
+
+    test('should return false as the name does not contain a special character', () => {
+        const result = isValid("peppermint7");
+        expect(result).toBeFalsy();
+    });
+
+    test('should return false as the name does not contain a number', () => {
+        const result = isValid("peppermint&");
+        expect(result).toBeFalsy();
+    });
+
+    test('should return true', () => {
+        const result = isValid("peppermint&1");
+        expect(result).toBeTruthy();
     });
 });
--- src/services/validations.js
+++ src/services/validations.js
@@ -1,4 +1,16 @@
 // TODO: Refactor validation function to follow the defined validations rules
-const isValid = () => Math.random() >= 0.5;
+const isValid = (gamerTag) => {
+    const searchChars = ["&", "$", "!", "è", "§", "à", "_"];
+    const containsChars = searchChars.every(char => gamerTag.includes(char));
+
+    const searchNumbers = ["1", "0", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const containsNumbers = searchNumbers.every(num => gamerTag.includes(num));
+
+    if(gamerTag.length()  < 8 || !containsChars || ! containsNumbers){
+        return false; 
+    }
+
+    return true;
+};
 
 exports.isValid = isValid;
